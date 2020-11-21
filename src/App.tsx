f735c010f06c22a39078a504cebcea0a9309300d
boyloe <boyloe@gmail.com>
2020-11-20 18:14:22 -0700

    center div

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './app.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Bryan Oyloe Portfolio Site</h1>
       <h2>Full Stack Software Engineer</h2>
-      <h2>Under Construction</h2>
+      <h2>Page Under Construction</h2>
     </div>
   );
 }
