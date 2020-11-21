8d37b6ed7b726277e60bfc32b54d380dd9cae9de
boyloe <boyloe@gmail.com>
2020-11-20 18:07:46 -0700

    Test Firebase Autodeploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Bryan Oyloe Portfolio Site</h1>
       <h2>Full Stack Software Engineer</h2>
       <h3>Testing</h3>
+      <h4>Autodeploy is working</h4>
     </div>
   );
 }
