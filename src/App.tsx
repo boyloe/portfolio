1323c401030e9d23a10c1269635d01401b02ff46
boyloe <boyloe@gmail.com>
2020-11-21 16:25:29 -0700

    Style personal card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import BryanPic from './assets/BryanNicePhoto.jpg'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bryan Oyloe Portfolio Site</h1>
-      <h2>Full Stack Software Engineer</h2>
-      <img src="https://cdn1.vectorstock.com/i/1000x1000/63/50/under-construction-sign-vector-21366350.jpg" alt="constructiong"/>
+    <div className="flex justify-center py-10">
+      <div className="max-w-md rounded overflow-hidden shadow-lg bg-gray-100">
+        <img className="w-full" src={BryanPic} alt="Bryan" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-blue-500 text-xl mb-2">
+            Bryan Oyloe - Full Stack Software Engineer
+          </div>
+          <p className="text-gray-700 text-base">
+            Full Stack Software engineer with a 9+ year background in Petroleum Engineering. 
+            Passionate about using my skills in React, React Native, Ruby, Rails, 
+            and Node to design creative solutions for clients. 
+          </p>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #FullStackEngineer
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #TypescriptDev
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">
+            #OutdoorLife
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
