cd5b55c00e932b7ce4563ba74f68cf6c5d100978
boyloe <boyloe@gmail.com>
2020-11-21 14:53:51 -0700

    Add font awesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <h1>Bryan Oyloe Portfolio Site</h1>
       <h2>Full Stack Software Engineer</h2>
-      <h2>Page Under Construction</h2>
       <img src="https://cdn1.vectorstock.com/i/1000x1000/63/50/under-construction-sign-vector-21366350.jpg" alt="constructiong"/>
     </div>
   );
