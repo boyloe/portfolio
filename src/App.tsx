62708c9cfa2ad315fdccc82cd2e4fc86f0189aab
boyloe <boyloe@gmail.com>
2020-11-20 18:11:00 -0700

    Add under construction

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ function App() {
     <div className="App">
       <h1>Bryan Oyloe Portfolio Site</h1>
       <h2>Full Stack Software Engineer</h2>
-      <h3>Testing</h3>
-      <h4>Autodeploy is working</h4>
+      <h2>Under Construction</h2>
     </div>
   );
 }
