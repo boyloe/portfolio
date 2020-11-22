916c3772874ca13d944fccd20019b04d38ab186f
boyloe <boyloe@gmail.com>
2020-11-22 10:11:00 -0700

    Change color styling on header and body

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,20 @@ import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="flex justify-center overflow-hidden py-10 container">
+    <div className="flex justify-center overflow-hidden py-10 ">
       <div className="max-w-xs rounded-lg  shadow-lg bg-gray-100">
         <img className="w-full rounded" src={BryanPic} alt="Bryan" />
         <div className="px-6 py-4">
-          <div className="font-bold text-blue-500 text-xl mb-2">
-            Bryan Oyloe - Full Stack Software Engineer
-          </div>
-          <p className="text-gray-700 text-base">
+          <h3 className="font-bold text-blue-900 text-2xl ">
+            Bryan Oyloe
+          </h3>
+          <h5 className="font-bold text-blue-800 text-sm mb-4">
+            Full Stack Software Engineer
+          </h5>
+          <p className="text-blue-900 text-base">
             Full Stack Software engineer with a 9+ year background in Petroleum Engineering. 
             Passionate about using my skills in React, React Native, Ruby, Rails, 
-            and Node to design creative solutions for clients. 
+            and Node to design creative solutions for my clients. 
           </p>
         </div>
         <div className="px-6 py-4 flex space-x-8">
