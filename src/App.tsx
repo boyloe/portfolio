b3de773f9c020a6778e77d5cbd19fda914c3c33f
boyloe <boyloe@gmail.com>
2020-11-22 22:01:44 -0700

    Remove height control from outer div

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <div className="flex justify-center border-solid border-2 border-black overflow-hidden py-12 bg-gradient-to-tl from-white via-gray-200 to-blue-300 w-screen h-screen">
-      <div className="max-w-xs max-h-72 rounded-xl shadow-lg bg-gray-100 border-solid border-2 border-black">
+      <div className="max-w-xs  rounded-xl shadow-lg bg-gray-100 border-solid border-2 border-black">
         <img className="w-full rounded-xl" src={BryanPic} alt="Bryan" />
         <div className="px-2 py-4">
           <h3 className="font-bold text-blue-900 text-2xl ">
