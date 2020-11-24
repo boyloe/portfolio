e49977bc47523a98da5b6524a7b9881930721b26
boyloe <boyloe@gmail.com>
2020-11-24 06:46:30 -0700

    Remove Empty code lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import BryanPic from './assets/BryanNicePhoto.jpg'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faDev, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
-
 
 function App() {
   return (
