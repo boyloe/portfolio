002e3537f8953d46e60056106ffaf3c1458aea9f
Bryan Oyloe <boyloe@gmail.com>
2020-12-17 15:47:39 -0700

    Add Mobile nav bar response

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 
 export const NavBar:React.FC = () => {
     return (
-        <nav className="bg-gray-300 text-black font-tech flex py-4 px-3 justify-end min-h-8">
-            <Link className="px-3 text-2xl mx-2 my-2" to='/'>Home</Link>
-            <Link className="px-3 text-2xl mx-2 my-2" to='/projects'>Projects</Link>
-            <Link className="px-3 text-2xl mx-2 my-2" to='/resume'>Resume</Link>
-            {/* <Link className="px-3 text-2xl mx-2 my-2" to='/blog'>Blog</Link> */}
-            <Link className="px-3 text-2xl mx-2 my-2" to='/contact'>Contact</Link>
-        </nav>
+        <div>
+            <nav className="bg-gray-300 text-black font-tech flex flex-col w-full md:flex-row py-4 px-3 justify-end min-h-8" >
+                <Link className="px-3 text-2xl mx-2 my-2" to='/'>Home</Link>
+                <Link className="px-3 text-2xl mx-2 my-2" to='/projects'>Projects</Link>
+                <Link className="px-3 text-2xl mx-2 my-2" to='/resume'>Resume</Link>
+                {/* <Link className="px-3 text-2xl mx-2 my-2" to='/blog'>Blog</Link> */}
+                <Link className="px-3 text-2xl mx-2 my-2" to='/contact'>Contact</Link>
+            </nav>            
+        </div>
     )
 }
