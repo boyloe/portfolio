f85f6d712eff0a852b055038d7b0b192d4fe6ff7
Bryan Oyloe <boyloe@gmail.com>
2020-12-20 09:05:27 -0700

    Increase margin on nav items

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -5,11 +5,11 @@ export const NavBar:React.FC = () => {
     return (
         <div>
             <nav className="bg-gray-300 text-black font-tech flex flex-col w-full md:flex-row py-4 px-3 justify-end min-h-8" >
-                <Link className="px-3 text-2xl mx-2 my-2" to='/'>Home</Link>
-                <Link className="px-3 text-2xl mx-2 my-2" to='/projects'>Projects</Link>
-                <Link className="px-3 text-2xl mx-2 my-2" to='/resume'>Resume</Link>
+                <Link className="px-3 text-2xl mx-3 my-2" to='/'>Home</Link>
+                <Link className="px-3 text-2xl mx-3 my-2" to='/projects'>Projects</Link>
+                <Link className="px-3 text-2xl mx-3 my-2" to='/resume'>Resume</Link>
                 {/* <Link className="px-3 text-2xl mx-2 my-2" to='/blog'>Blog</Link> */}
-                <Link className="px-3 text-2xl mx-2 my-2" to='/contact'>Contact</Link>
+                <Link className="px-3 text-2xl mx-3 my-2" to='/contact'>Contact</Link>
             </nav>            
         </div>
     )
