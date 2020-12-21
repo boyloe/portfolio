4305c026e80213fa743480ab6603b3618bb3878e
Bryan Oyloe <boyloe@gmail.com>
2020-12-21 12:43:23 -0700

    Adjust styling for lg screens

--- src/components/HomePage/IntroBox.tsx
+++ src/components/HomePage/IntroBox.tsx
@@ -3,7 +3,7 @@ import { ReactTypical } from '@deadcoder0904/react-typical'
 
 export const IntroBox:React.FC = () => {
     return (
-        <div className="h-1/3 w-96 flex flex-col justify-center px-6 mt-10">
+        <div className="h-1/3 w-full flex flex-col justify-center md:items-start px-6 mt-10 md:h-1/4 md:mt-36 md:w-1/2">
             <div className="">
                 <ReactTypical 
                     className="text-3xl py-2 font-tech text-black" 
