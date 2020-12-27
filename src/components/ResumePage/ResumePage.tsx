ea7c420b6913a0febe97f3ab1604435b6604a04e
Bryan Oyloe <boyloe@gmail.com>
2020-12-27 08:10:07 -0600

    Add resume header

--- src/components/ResumePage/ResumePage.tsx
+++ src/components/ResumePage/ResumePage.tsx
@@ -11,9 +11,8 @@ export const ResumePage:React.FC = () => {
 
     return (
     <div className="h-screen bg-blue-line-wave bg-cover flex justify-center">
-
-            <iframe title='resume' className="h-1/3 transform scale-150" src={Resume}></iframe>            
-
+        <iframe title='resume' className="h-1/3 transform scale-150" src={Resume}></iframe>    
+        <h1 className="text-2xl transform -rotate-90">Resume</h1>
     </div>
     )
 }
