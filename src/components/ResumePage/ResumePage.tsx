80b89ccd30817cf739a8157404a6f615cc73c988
Bryan Oyloe <boyloe@gmail.com>
2020-12-28 08:01:25 -0600

    translate resume title

--- src/components/ResumePage/ResumePage.tsx
+++ src/components/ResumePage/ResumePage.tsx
@@ -10,9 +10,9 @@ export const ResumePage:React.FC = () => {
 
 
     return (
-    <div className="h-screen bg-blue-line-wave bg-cover flex justify-center">
+    <div className="h-screen bg-blue-line-wave bg-cover flex flex-row justify-center">
         <iframe title='resume' className="h-1/3 transform scale-150" src={Resume}></iframe>    
-        <h1 className="text-2xl transform -rotate-90">Resume</h1>
+        <h1 className="text-2xl transform -rotate-90 -translate-x-32">Resume</h1>
     </div>
     )
 }
