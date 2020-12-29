114eaebc22e5a490a09b8776e7e8927d72868685
Bryan Oyloe <boyloe@gmail.com>
2020-12-29 10:08:43 -0700

    Adjust x-translate on resume title

--- src/components/ResumePage/ResumePage.tsx
+++ src/components/ResumePage/ResumePage.tsx
@@ -12,7 +12,7 @@ export const ResumePage:React.FC = () => {
     return (
     <div className="h-screen bg-blue-line-wave bg-cover flex flex-row justify-center">
         <iframe title='resume' className="h-1/3 transform scale-150" src={Resume}></iframe>    
-        <h1 className="text-2xl transform -rotate-90 -translate-x-32">Resume</h1>
+        <h1 className="text-2xl transform -rotate-90 -translate-x-36">Resume</h1>
     </div>
     )
 }
